993c8560d978cb935f9830a71e48e55b9d06753e
waseem i <waseemmachloy@gmail.com>
2017-01-12 23:58:32 +0300

    Preferences Seeder Added

---
+++ database/seeds/ChitchatPreferenceTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ChitchatPreferenceTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
