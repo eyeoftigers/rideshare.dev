a0b0ba0f97f50a217bdb5ad694ed3443da1bff68
waseem i <waseemmachloy@gmail.com>
2017-01-16 20:41:17 +0300

    faviico added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'LOCAL') }}</title>
+    <link rel="shortcut icon" href="/favicon.ico" />
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
